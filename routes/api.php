9e227a762e555a3661c94eb8e188bff45041471b
Gerson Manuel Sugianto <manuelsugianto@gmail.com>
2025-05-01 04:29:45 +0700

    Move sheet data API routes from api.php to web.php

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\SheetSyncController;
-
-Route::get('/sheet-data', [SheetSyncController::class, 'getData']);
-Route::post('/sheet-data', [SheetSyncController::class, 'updateData']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SheetSyncController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sheet-data', [SheetSyncController::class, 'getData']);
+Route::post('/sheet-data', [SheetSyncController::class, 'updateData']);
